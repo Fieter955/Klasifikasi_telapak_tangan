49d182a251a0f199b833459fd24aa9141cad4993
Fieter <127859135+Fieter955@users.noreply.github.com>
2026-05-13 18:31:39 +0800

    fix delay in railway 3

--- main.py
+++ main.py
@@ -464,7 +464,3 @@ if __name__ == "__main__":
     # Railway/Cloud biasanya memberikan port melalui environment variable PORT
     port = int(os.environ.get("PORT", 8011))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
- import os
-    # Railway/Cloud biasanya memberikan port melalui environment variable PORT
-    port = int(os.environ.get("PORT", 8011))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
